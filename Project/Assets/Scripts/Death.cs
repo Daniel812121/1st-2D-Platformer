be71f84c0bb76be5ed28db3045639b327cf2ba9c
Daniel812121 <58784062+Daniel812121@users.noreply.github.com>
2020-05-04 11:56:55 -0400

    More Health
    More Health stuff

---
+++ Project/Assets/Scripts/Death.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //health--;
+        void Update()
+        {
+            if (transform.position.y == -20)
+            {
+                health--;
+            }
+            if (health == 3)
+            {
+
+            }
+            else if (health == 2)
+            {
+                heart3.SetActive(false);
+            }
+            else if (health == 1)
+            {
+                heart3.SetActive(false);
+                heart2.SetActive(false);
+            }
+
+
+        }
+    }
+}
---
+++ Project/Assets/Scripts/PlayerHealthTheSequel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthTheSequel : MonoBehaviour
+{
+    GameObject heart3;
+    GameObject heart2;
+
+    private int health = 3;
+    void Start()
+    {
+        heart3.SetActive(true);
+        heart2.SetActive(true);
+    }
+    void Update()
+    {
+        if (transform.position.y == -20)
+        {
+            health--;
+        }
+
+        if (health == 3)
+        {
+            //things
+        }
+        else if (health == 2)
+        {
+            heart3.SetActive(false);
+        }
+        else if (health == 1)
+        {
+            heart3.SetActive(false);
+            heart2.SetActive(false);
+        }
+}
